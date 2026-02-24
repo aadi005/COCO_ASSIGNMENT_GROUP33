9bc2863c090ba706966a748d3b409ea4ba8072a9
Aaditya Goel <aadityagoel05@gmail.com>
2026-02-24 10:20:23 +0530

    Initializing Repository

---
+++ src/lexer.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lexerDef.h"
+#include "stateHandlers.h"
+
+// Example usage within lexer.c
+void getNextChar(FILE *fp) {
+    // Uses fgetc from stdio.h to fill the twin buffers
+}
