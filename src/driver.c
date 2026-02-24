4dc47c1657984be43648e9333a678423c35a46ae
Prakhar072 <f20220426@pilani.bits-pilani.ac.in>
2026-02-24 11:22:59 +0530

    changed main.c to driver.c added functionality

---
+++ src/driver.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "lexer.h"
+/* Parser will be added later */
+#include "parser.h"   /* Leave this include for later implementation */
+
+/* ===========================
+   STATUS DISPLAY FUNCTION
+   =========================== */
+
+void printImplementationStatus() {
+
+    printf("=============================================\n");
+    printf("Compiler Construction Project - Status\n");
+    printf("=============================================\n");
+
+    printf("Lexical Analyzer Module: IMPLEMENTED\n");
+    printf("FIRST and FOLLOW: Pending / To be implemented\n");
+    printf("Predictive Parser: Pending\n");
+    printf("Parse Tree Construction: Pending\n");
+    printf("Error Recovery: Pending\n");
+
+    printf("=============================================\n\n");
+}
+
+
+/* ===========================
+   MAIN DRIVER
+   =========================== */
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 3) {
+        printf("Usage: %s <sourcecodefile> <parsetreeoutfile>\n", argv[0]);
+        return 1;
+    }
+
+    char *sourceFile = argv[1];
+    char *parseTreeOutFile = argv[2];
+
+    int option;
+
+    printImplementationStatus();
+
+    while (1) {
+
+        printf("\n=========== MENU ===========\n");
+        printf("0 : Exit\n");
+        printf("1 : Remove comments\n");
+        printf("2 : Print token list\n");
+        printf("3 : Parse and print parse tree\n");
+        printf("4 : Print total execution time\n");
+        printf("============================\n");
+
+        printf("Enter option: ");
+        scanf("%d", &option);
+
+        if (option == 0) {
+            printf("Exiting...\n");
+            break;
+        }
+
+        switch (option) {
+
+            /* ========================================= */
+            /* OPTION 1 - REMOVE COMMENTS                */
+            /* ========================================= */
+
+            case 1: {
+                printf("\nRemoving comments...\n");
+                removeComments(sourceFile, "cleaned_output.txt");
+                printf("Comment-free code printed to console.\n");
+                break;
+            }
+
+            /* ========================================= */
+            /* OPTION 2 - PRINT TOKEN LIST               */
+            /* ========================================= */
+
+            case 2: {
+                printf("\nPerforming Lexical Analysis...\n");
+
+                FILE *fp = fopen(sourceFile, "r");
+                if (!fp) {
+                    printf("Error opening source file.\n");
+                    break;
+                }
+
+                twinBuffer *tb = initializeLexer(fp);
+                tokenInfo tk;
+
+                printf("\n%-20s %-20s %-10s\n", "LEXEME", "TOKEN", "LINE NO");
+                printf("------------------------------------------------------------\n");
+
+                while (1) {
+
+                    tk = getNextToken(tb);
+
+                    if (tk.token == TK_EOF)
+                        break;
+
+                    printToken(tk);
+                }
+
+                fclose(fp);
+                break;
+            }
+
+            /* ========================================= */
+            /* OPTION 3 - PARSER (PLACEHOLDER)           */
+            /* ========================================= */
+
+            case 3: {
+                printf("\nParser module not implemented yet.\n");
+                printf("This option will:\n");
+                printf("- Invoke lexer\n");
+                printf("- Build parse tree\n");
+                printf("- Print errors\n");
+                printf("- Output parse tree to file: %s\n", parseTreeOutFile);
+                break;
+            }
+
+            /* ========================================= */
+            /* OPTION 4 - TIME MEASUREMENT               */
+            /* ========================================= */
+
+            case 4: {
+                printf("\nMeasuring execution time (Lexer + Parser)...\n");
+
+                clock_t start_time, end_time;
+                double total_CPU_time, total_CPU_time_in_seconds;
+
+                start_time = clock();
+
+                /* Lexer invocation */
+                FILE *fp = fopen(sourceFile, "r");
+                if (!fp) {
+                    printf("Error opening source file.\n");
+                    break;
+                }
+
+                twinBuffer *tb = initializeLexer(fp);
+                tokenInfo tk;
+
+                while (1) {
+                    tk = getNextToken(tb);
+                    if (tk.token == TK_EOF)
+                        break;
+                }
+
+                fclose(fp);
+
+                /* Parser will be invoked here later */
+
+                end_time = clock();
+
+                total_CPU_time = (double)(end_time - start_time);
+                total_CPU_time_in_seconds = total_CPU_time / CLOCKS_PER_SEC;
+
+                printf("Total CPU Time (ticks): %lf\n", total_CPU_time);
+                printf("Total CPU Time (seconds): %lf\n", total_CPU_time_in_seconds);
+
+                break;
+            }
+
+            default:
+                printf("Invalid option. Try again.\n");
+        }
+    }
+
+    return 0;
+}
